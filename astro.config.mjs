1b5d59d7f4bdcaa20395754cfa062af07517cc56
0xcire <ciredeveloper@gmail.com>
2023-07-25 14:05:25 -0400

    set max width for users on larger devices

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://0xcire.github.io",
-  base: "/food_service",
+  base: "/food-service",
 });
